d2458589abed294841d353b48a4de57cc163931f
guofei9987 <guofei9987@foxmail.com>
2022-07-09 23:04:57 +0800

    first lines

--- examples/example1.py
+++ examples/example1.py
@@ -5,3 +5,7 @@ lsh.index([1, 2, 3, 4, 5, 6, 7, 8])
 lsh.index([2, 3, 4, 5, 6, 7, 8, 9])
 lsh.index([10, 12, 99, 1, 5, 31, 2, 3])
 lsh.query([1, 2, 3, 4, 5, 6, 7, 7])
+
+# %%
+lsh.save_uniform_planes("filename.pkl")
+lsh.load_uniform_planes("filename.pkl")
