54aa9dc8b42c55ef75884b4b76278eb129052b69
郭飞 <guofei9987@foxmail.com>
2023-07-04 14:13:21 +0000

    add fuzzy-hash

--- pyLSHash/__init__.py
+++ pyLSHash/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+__version__ = '0.1.1'
 
 from pyLSHash.lshash import LSHash
 from .distance import hamming
 from .simhash import SimHash
-
-__version__ = '0.1.1'
+try:
+    from fuzzy_hash import fuzzy_hash, fuzzy_compare
+except:
+    print("Do:\n `pip install fuzzy-hash`")
