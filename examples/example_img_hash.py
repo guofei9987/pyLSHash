f911418a82eb51e5d606a7ed31dd1cb8d13685f2
guofei9987 <guofei9987@foxmail.com>
2023-11-25 17:06:01 +0800

    update examples

--- examples/example_img_hash.py
+++ examples/example_img_hash.py
@@ -6,6 +6,7 @@ import PIL
 img1 = 'img.jpeg'
 img2 = 'att_img.jpeg'
 att.resize_att(input_filename=img1, output_file_name=img2, out_shape=(300, 500))
+img1_att_noise = att.shelter_att(input_filename=img1, output_file_name='img_with_noise.jpeg')
 
 # %% aHash
 a_hash_img1 = img_hash.a_hash(PIL.Image.open(img1))
@@ -13,7 +14,7 @@ a_hash_img2 = img_hash.a_hash(PIL.Image.open(img2))
 
 hamming_distance = hamming(a_hash_img1, a_hash_img2)
 print('[aHash]: img1 = {}, img2 = {}'.format(hex(a_hash_img1), hex(a_hash_img2)))
-print(f'hamming_distance = {hamming_distance}')
+print(f'[aHash] hamming_distance = {hamming_distance}')
 assert hamming_distance < 5
 
 # %% dHash
@@ -22,7 +23,7 @@ d_hash_img2 = img_hash.d_hash(PIL.Image.open(img2))
 
 hamming_distance = hamming(d_hash_img1, d_hash_img2)
 print('[dHash]: img1 = {}, img2 = {}'.format(hex(d_hash_img1), hex(d_hash_img2)))
-print(f'hamming_distance = {hamming_distance}')
+print(f'[aHash] hamming_distance = {hamming_distance}')
 assert hamming_distance < 5
 
 # %% pHash
@@ -31,16 +32,15 @@ p_hash_img2 = img_hash.p_hash(PIL.Image.open(img2))
 
 hamming_distance = hamming(p_hash_img1, p_hash_img2)
 print('[pHash]: img1 = {}, img2 = {}'.format(hex(p_hash_img1), hex(p_hash_img2)))
-print(f'hamming_distance = {hamming_distance}')
+print(f'[pHash] hamming_distance = {hamming_distance}')
 assert hamming_distance < 5
 
 # %% SSIM
 import cv2
 from pyLSHash.img_ssim import SSIM
 
-img1_att_noise = att.shelter_att(input_filename=img1)
-
 ssim = SSIM()
-ssim_score = ssim.cal_ssim(cv2.imread(img1), img1_att_noise)
+
+ssim_score = ssim.cal_ssim(cv2.imread(img1), cv2.imread('img_with_noise.jpeg'))
 
 print("SSIM after attack:", ssim_score)
