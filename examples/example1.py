f34e2df6762e95df629d05667b475ee9dfabb9cb
guofei9987 <guofei9987@foxmail.com>
2023-12-24 19:32:55 +0800

    fix a bug in example

--- examples/example1.py
+++ examples/example1.py
@@ -12,10 +12,13 @@ lsh.index([10, 12, 99, 1, 5, 31, 2, 3])
 lsh.index([10, 12, 99, 1, 5, 31, 2, 3])
 
 # test hamming_dist
-res2 = lsh.query([1, 2, 3, 4, 5, 6, 7, 7], key_hamming=True)
+res = lsh.query([1, 2, 3, 4, 5, 6, 7, 7])
 
 print(res)
 
+assert res[0][0][0] == (1, 2, 3, 4, 5, 6, 7, 8)
+
+res2 = lsh.query([1, 2, 3, 4, 5, 6, 7, 7], key_hamming=True)
 assert res2[0][0][0] == (1, 2, 3, 4, 5, 6, 7, 8)
 
 
